20c9ebcf9b209daeba6ddd97d6072d159488a349
Fantine Rudent <fantine.rudent@gmail.com>
2024-03-21 11:58:15 +0100

    change

--- html/index.js
+++ html/index.js
@@ -1,9 +1,3 @@
-const modalOne = document.getElementById("art1-mod");
-const modalTwo = document.getElementById("art2-mod");
-const modalThree = document.getElementById("art3-mod");
 
-const btnOne = document.getElementById("infoOne");
-const btnTwo = document.getElementById("infoTwo");
-const btnThree = document.getElementById("infoThree");
 
 // Ajout des animations pour afficher les modals.
