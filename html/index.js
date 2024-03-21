3516771e5cfa604e8c3d305c94e1cf59a7c4efb1
Fantine Rudent <fantine.rudent@gmail.com>
2024-03-21 12:30:44 +0100

    maj

--- html/index.js
+++ html/index.js
@@ -1,3 +1,3 @@
-
-
 // Ajout des animations pour afficher les modals.
+// pour l'instant les modales sont visibles, je veux qu'elles soit invisibles quand j'arrive sur la page, 
+// mais qu'elles s'affichent lorsque je clique sur le bouton au dessus d'elle! :)
